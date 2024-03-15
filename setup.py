2eb1bd750c1060f174cf950e87d18f5692ddc6c4
dream同学0 <1399541701@qq.com>
2024-03-15 18:10:10 +0800

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
 setuptools.setup(
     name="local-augment",
-    version="0.0.1",
+    version="0.1.1",
     license="MIT license",
     author="TayDream",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email="19859860010@163.com",
     description="A package for local data augmentation in deep learning",
     url="https://github.com/dream-oyh/LocalAugment",
